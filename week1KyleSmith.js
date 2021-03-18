b8795d85c11e24d167b8d7f8856d1392393716b7
KyleSmithpc22 <kylepc22!@gmail.com>
2021-03-18 19:56:41 -0400

    week1 assignment

---
+++ week1KyleSmith.js
@@ -0,0 +1,22 @@
+var firstName = "Kyle";
+var lastName = "Smith";
+var middleInitial = 'J';
+var ageInYears = 21;
+var year = 1999;
+
+var itemprice = 12.99;
+var amountOfMoneyInWallet = 50.00;
+
+var numberOfFriends = 3;
+var numberOfFriendsGained = ageInYears*2;
+var finalNumberOfFriends = numberOfFriends + numberOfFriendsGained;
+
+var fullName = firstName + " " + middleInitial + " " + lastName;
+var newAmountOfMoney = amountOfMoneyInWallet - itemprice;
+
+
+console.log("My fullname is " + fullName + ".");
+console.log("I have " + newAmountOfMoney + " in my wallet after my purchase.");
+console.log("I have " + finalNumberOfFriends + " friends after all these years.");
+
+
