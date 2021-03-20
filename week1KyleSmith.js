0f264ec1ee06058bc0092b3e5c32a0f3e7dedc5e
KyleSmithpc22 <kylepc22!@gmail.com>
2021-03-20 13:36:54 -0400

    Week1 assignment

--- week1KyleSmith.js
+++ week1KyleSmith.js
@@ -1,20 +1,24 @@
+// About me variables
 var firstName = "Kyle";
 var lastName = "Smith";
 var middleInitial = 'J';
 var ageInYears = 21;
-var year = 1999;
 
+// Item price and money variables
 var itemprice = 12.99;
 var amountOfMoneyInWallet = 50.00;
 
+// freinds and freinds gained by my age variables and math
 var numberOfFriends = 3;
 var numberOfFriendsGained = ageInYears*2;
 var finalNumberOfFriends = numberOfFriends + numberOfFriendsGained;
 
+// putting the 3 parts of my name into 1
 var fullName = firstName + " " + middleInitial + " " + lastName;
+// setting my money to a lower amount due to the purchesing of the item
 var newAmountOfMoney = amountOfMoneyInWallet - itemprice;
 
-
+// the logs that will display when the consel is opened within the deafualt brosers with the approprat link.
 console.log("My fullname is " + fullName + ".");
 console.log("I have " + newAmountOfMoney + " in my wallet after my purchase.");
 console.log("I have " + finalNumberOfFriends + " friends after all these years.");
